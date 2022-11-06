c7ccf9239c99e57fb5e8cc345696e45dd36c5067
DouglasNeres <douglasneres06@gmail.com>
2022-11-06 15:29:34 -0300

    Nlw - Google authentication implementation

--- ignite-trail/my-mobile/src/contexts/AuthContext.tsx
+++ ignite-trail/my-mobile/src/contexts/AuthContext.tsx
@@ -1,41 +1,72 @@
-import { createContext, ReactNode } from "react";
+import { createContext, ReactNode, useState, useEffect } from "react";
+import * as Google from 'expo-auth-session/providers/google'
 import * as AuthSession from 'expo-auth-session'
+import * as WebBrowser from 'expo-web-browser'
 
-interface UserProps{
+WebBrowser.maybeCompleteAuthSession()
+
+
+interface UserProps {
     name: string;
     avatarUrl: string;
 }
 
 export interface AuthContextDataProps {
     user: UserProps;
+    isUserLoading: boolean;
     signIn: () => Promise<void>
 }
 
-interface AuthProviderProps{
+interface AuthProviderProps {
     children: ReactNode
 }
 
 export const AuthContext = createContext({} as AuthContextDataProps)
 
 export function AuthContextProvider({ children }: AuthProviderProps) {
+    const [user, setUser] = useState<UserProps>({} as UserProps)
+    const [isUserLoading, setIsUserLoading] = useState(false)
 
-    console.log(AuthSession.makeRedirectUri({useProxy: true}))
+    const [request, response, promptAsync] = Google.useAuthRequest({
+        clientId: '378868385341-glk3ccuksnnu7071oao8fu7vafiqlfsk.apps.googleusercontent.com',
+        redirectUri: AuthSession.makeRedirectUri({ useProxy: true }),
+        scopes: ['profile', 'email']
+    })
 
     async function signIn() {
-        console.log('Vamos Logar!');
+        try {
+            setIsUserLoading(true)
+            await promptAsync()
+        } catch (err) {
+            console.log(err);
+            throw err;
+
+        } finally {
+            setIsUserLoading(false)
+        }
+    }
+
+    async function signInWithGoogle(access_token: string) {
+        console.log("TOKEN DE AUTENTICAÇÃO ===>", access_token);
         
     }
 
-    return(
+    useEffect(() =>{
+        if(response?.type === 'success' && response.authentication?.accessToken){
+            signInWithGoogle(response.authentication.accessToken)
+        }
+
+    }, [response])
+
+
+    return (
         <AuthContext.Provider value={{
             signIn,
-            user:{
-                name: 'Douglas Neres',
-                avatarUrl: 'https://github.com/DouglasNeres.png'
-            }
+            isUserLoading,
+            user,
         }}>
 
-        {children}
+            {children}
         </AuthContext.Provider>
     )
 }
