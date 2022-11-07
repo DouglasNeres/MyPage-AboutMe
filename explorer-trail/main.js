8e6d895d83854ded067e248ca91846972c54e5d1
DouglasNeres <douglasneres06@gmail.com>
2022-11-07 01:14:47 -0300

    Nlw - Adding refactoring, animation and responsiveness

--- explorer-trail/main.js
+++ explorer-trail/main.js
@@ -9,9 +9,11 @@ function createGames(player1, hour, player2) {
     `;
 }
 
+let delay = -0.4
 function createCard(date, day, games) {
+  delay = delay + 0.4
   return `
-    <div class="card">
+    <div class="card" style="animation-delay: ${delay}s">
     <h2>${date} <span>${day}</span></h2>
     <ul>
       ${games}
@@ -20,23 +22,20 @@ function createCard(date, day, games) {
     `;
 }
 
-document.querySelector("#app").innerHTML = `
-<header>
-<img src="./assets/logo.svg" alt="Logo da NLW" />
-</header>
-<main id="cards">
-    ${createCard("24/11", "quinta", createGames("switzerland", "07:00", "cameroon")
+document.querySelector("#cards").innerHTML =
+
+    createCard("24/11", "quinta", createGames("switzerland", "07:00", "cameroon")
   + createGames("uruguay", "10:00", "south korea")
   + createGames("portugal", "13:00", "ghana")
-  + createGames("brazil", "16:00", "serbia"))}
-    ${createCard("28/11", "segunda", createGames("serbia", "07:00", "cameroon")
+  + createGames("brazil", "16:00", "serbia")) +
+
+    createCard("28/11", "segunda", createGames("serbia", "07:00", "cameroon")
     + createGames("south korea", "10:00", "ghana")
     + createGames("brazil", "13:00", "switzerland")
-    + createGames("portugal", "16:00", "uruguay"))}
-    ${createCard("02/12", "sexta", createGames("south korea", "12:00", "portugal")
+    + createGames("portugal", "16:00", "uruguay")) +
+    
+    createCard("02/12", "sexta", createGames("south korea", "12:00", "portugal")
       + createGames("ghana", "12:00", "uruguay")
       + createGames("serbia", "16:00", "switzerland")
-      + createGames("brazil", "16:00", "cameroon"))} 
+      + createGames("brazil", "16:00", "cameroon")) 
     
-</main>
-`;
